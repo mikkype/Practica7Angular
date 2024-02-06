3d4f78806b5008449a4d3eb167e18adcd294f9ac
mikkype <miguelcaceda@gmail.com>
2024-02-06 21:37:14 +0100

    store app

--- src/app/stock/stock.component.ts
+++ src/app/stock/stock.component.ts
@@ -7,47 +7,54 @@ import { Product } from '../models/product.model';
   styleUrl: './stock.component.css'
 })
 export class StockComponent {
-public product: Product = new Product(0,"",0,0);
+  public product: Product = new Product(0, "", 0, 0);
 
-public products : Product []= [
-  new Product (1,'mobile lcd',5,50),
-  new Product (2,'port usb',8,30),
-  new Product (3,'b7000-glue',6,10),
-  new Product (4,'cable flex',10,30),
-  new Product (5,'mobile tool kit',9,25)
-  
-]
+  public products: Product[] = [
+    new Product(1, 'mobile lcd', 5, 50),
+    new Product(2, 'port usb', 8, 30),
+    new Product(3, 'b7000-glue', 6, 10),
+    new Product(4, 'cable flex', 10, 30),
+    new Product(5, 'mobile tool kit', 9, 25)
 
-addProduct(){
-  this.products.push(this.product);
-  this.product = new Product(0,"",0,0);
-  
-}
+  ]
 
-
-deleteProduct(product: Product) {
-  // Recorrer el array de productos con foreach
-  this.products.forEach((p, index) => {
-    // Si el producto coincide con el parámetro, eliminarlo con splice
-    if (p.code === product.code) {
-      this.products.splice(index, 1);
-    }
-  });
-}
-
-
-modifyProduct(){
-  var index = this.products.findIndex(p => p.code === this.product.code);
-  if(index!==-1) {
-    this.products[index] = this.product;
+  deleteProduct2(product: Product) {
+    this.products.forEach((p, index) => {
+      if (p.code === product.code) {
+        this.products.splice(index, 1);
+      }
+    });
   }
-  this.product = new Product (0,'',0,0);
-}
-selectProduct(product:Product){
-  this.product = product;
+
+  addProduct() {
+    this.products.push(this.product);
+    this.product = new Product(0, "", 0, 0);
+  }
+
+  deleteProduct(product: Product) {
+    this.products.forEach((p, index) => {
+      if (p.code === product.code) {
+        this.products.splice(index, 1);
+      }
+    });
+  }
+
+
+  modifyProduct() {
+    var index = this.products.findIndex(p => p.code === this.product.code);
+    if (index !== -1) {
+      this.products[index] = this.product;
+    }
+    else {
+      this.product = new Product(0, '', 0, 0);
+    }
+
+  }
+
+  selectProduct(product: Product) {
+    this.product = product;
+  }
+
 
 }
-
-}
-
 
